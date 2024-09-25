b30179eb980d8af3f17ae2d233c3fd3a7d1123fd
mariam-khasawneh <khasawnehmariam@gmail.com>
2024-09-25 10:36:48 +0300

    final

---
+++ backend/routes/userRoutesAdmin.js
@@ -0,0 +1,16 @@
+// userRoutes.js
+const express = require("express");
+const {
+  getUsers,
+  toggleUserStatus,
+} = require("../controllers/userControllerAdmin");
+
+const router = express.Router();
+
+// Route to get all users
+router.get("/users/admin", getUsers);
+
+// Route to toggle user's isActive status
+router.put("/users/admin/:id/status", toggleUserStatus);
+
+module.exports = router;
